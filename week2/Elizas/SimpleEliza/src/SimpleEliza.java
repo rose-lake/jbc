b45bb16c52b48c20611f000d0012ebe6002f80a3
rose-lake <klake12@montgomerycollege.edu>
2019-10-29 14:32:17 -0400

    simple eliza works

--- week2/Elizas/SimpleEliza/src/SimpleEliza.java
+++ week2/Elizas/SimpleEliza/src/SimpleEliza.java
@@ -5,10 +5,35 @@
  *
  * see README for project description
  */
+
+import java.util.Scanner;
+
 public class SimpleEliza {
 
     public static void main(String[] args) {
-        
+
+        boolean play = true;
+        Scanner keyboard = new Scanner(System.in);
+        String answer;
+
+        // ****************************
+        //  main running loop
+        // ****************************
+        System.out.print("Good day. What is your problem? ");
+        while(true) {
+
+            System.out.print("Enter your response here or \"Q\" to quit: ");
+
+            answer = keyboard.nextLine();
+
+            System.out.println(answer);
+
+            if (answer.equalsIgnoreCase("q") || answer.equalsIgnoreCase("i am feeling great")) {
+                break;
+            }
+        }
+        System.out.println(">>> END");
+
     }
 
 }
