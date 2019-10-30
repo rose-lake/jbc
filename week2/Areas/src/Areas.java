5a38e11bd25991724a37b2646f5437abb165ca92
rose-lake <klake12@montgomerycollege.edu>
2019-10-30 10:46:58 -0400

    working Areas example

--- week2/Areas/src/Areas.java
+++ week2/Areas/src/Areas.java
@@ -10,33 +10,78 @@ public class Areas {
 
     public static void main(String[] args){
 
+        // choosing not to wrap in a while(true) "play" loop this time
         System.out.println("Welcome to Areas!");
         System.out.print("What type of area would you like to calculate?" +
-                "\n\t(1) circle" +
-                "\n\t(2) triangle" +
-                "\n\t(3) rectangle" +
-                "\n\t(4) square" +
+                "\n\t1: circle" +
+                "\n\t2: triangle" +
+                "\n\t3: rectangle" +
+                "\n\t4: square" +
                 "\nPlease enter your choice: ");
 
         Scanner keyboard = new Scanner(System.in);
-        String answer = keyboard.nextLine();
+        int choice = keyboard.nextInt();                // choosing not to wrap in error-handler this time
+        keyboard.nextLine();
 
+        switch (choice) {
+            case 1:
+                circleArea();
+                break;
+            case 2:
+                triangleArea();
+                break;
+            case 3:
+                rectangleArea();
+                break;
+            case 4:
+                squareArea();
+                break;
+            default:
+                System.out.println("You did not enter a valid choice...");
+        }
+
+        System.out.println("Goodbye!");
+        keyboard.close();
 
     }
 
-    private static double circleArea(double radius) {
-        return (radius * radius) * 3.14;
+    private static void circleArea() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter a value for the radius (type is double): ");
+        double radius = keyboard.nextDouble();
+        keyboard.nextLine();
+        System.out.println("The area of your circle is: " + ((radius * radius) * 3.14));
     }
 
-    private static double triangleArea(double base, double height) {
-        return (base * height) / 2;
+    private static void triangleArea() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter values for the base and the height (type is double)...");
+        System.out.print("\tbase: ");
+        double base = keyboard.nextDouble();
+        keyboard.nextLine();
+        System.out.print("\theight: ");
+        double height = keyboard.nextDouble();
+        keyboard.nextLine();
+        System.out.println("The area of your triangle is: " + ((base * height)/2));
     }
 
-    private static double rectangleArea(double base, double height) {
-        return base * height;
+    private static void rectangleArea() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter values for the base and the height (type is double)...");
+        System.out.print("\tbase: ");
+        double base = keyboard.nextDouble();
+        keyboard.nextLine();
+        System.out.print("\theight: ");
+        double height = keyboard.nextDouble();
+        keyboard.nextLine();
+        System.out.println("The area of your rectangle is: " + ((base * height)));
     }
 
-    private static double squareArea(double side) {
-        return side * side;
+    private static void squareArea() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter a value for the side (type is double): ");
+        double side = keyboard.nextDouble();
+        keyboard.nextLine();
+        System.out.println("The area of your square is: " + ((side * side)));
     }
 }
