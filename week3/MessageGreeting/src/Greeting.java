4a67bcc533b921e577d3a74b5e2fb0278e8d6d40
rose-lake <klake12@montgomerycollege.edu>
2019-11-01 16:30:37 -0400

    OO examples

--- week3/MessageGreeting/src/Greeting.java
+++ week3/MessageGreeting/src/Greeting.java
@@ -1,2 +1,36 @@
+
 public class Greeting {
+
+    private String greeting;
+    private String sender;
+
+    // Constructor runs when the keyword 'new' is encountered
+    public Greeting(){
+        System.out.println("The constructor has run");
+    }
+    // Overloaded constructor allows us to pass parameters
+    public Greeting(String greeting, String sender){
+        this.greeting = greeting;
+        this.sender = sender;
+    }
+
+    public String displayMessage(){
+        return sender + " says " + greeting;
+    }
+
+    // getters and setters
+    public String getGreeting(){
+        return greeting;
+    }
+    public void setGreeting(String greeting){
+        this.greeting = greeting;
+    }
+
+    public String getSender(){
+        return sender;
+    }
+    public void setSender(String sender){
+        this.sender = sender;
+    }
+
 }
--- week3/MessageGreeting/src/Main.java
+++ week3/MessageGreeting/src/Main.java
@@ -1,2 +1,23 @@
+
 public class Main {
+
+    public static void main(String[] args){
+
+        Greeting g = new Greeting();
+        g.setSender("Dave");
+        g.setGreeting("Hello");
+        String message = g.displayMessage();
+        System.out.println(message);
+
+        Greeting g2 = new Greeting();
+        g2.setSender("Melissa");
+        g2.setGreeting("Hello from Melissa");
+        System.out.println(g2.getSender());
+        System.out.println(g2.getGreeting());
+        System.out.println(g2.displayMessage());
+
+        Greeting g3 = new Greeting("I like Java", "Jennifer");
+        System.out.println(g3.displayMessage());
+    }
+
 }
