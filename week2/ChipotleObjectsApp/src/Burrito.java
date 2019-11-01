b476462044c5ba655f52bc1a5c718724b4eda01a
rose-lake <klake12@montgomerycollege.edu>
2019-11-01 13:25:50 -0400

    basic structure

--- week2/ChipotleObjectsApp/src/Burrito.java
+++ week2/ChipotleObjectsApp/src/Burrito.java
@@ -2,5 +2,68 @@
 
 public class Burrito {
 
+    //*****************
+    // class variables
+    //*****************
+    private String rice;
+    private String bean;
+    private String meat;
+    private String salsa;
+    private String veggies;
+
+    //*****************
+    // constructors
+    //*****************
+
+    // default constructor
+    public Burrito(){
+    }
+
+    // overloaded constructor
+    public Burrito(String rice, String bean, String meat, String salsa, String veggies){
+        this.rice = rice;
+        this.bean = bean;
+        this.meat = meat;
+        this.salsa = salsa;
+        this.veggies = veggies;
+    }
+
+    //*********************
+    // getters and setters
+    //**********************
+    public String getRice(){
+        return this.rice;
+    }
+    public void setRice(String rice){
+        this.rice = rice;
+    }
+
+    public String getBean(){
+        return this.bean;
+    }
+    public void setBean(String bean){
+        this.bean = bean;
+    }
+
+    public String getMeat(){
+        return this.meat;
+    }
+    public void setMeat(String meat){
+        this.meat = meat;
+    }
+
+    public String getSalsa(){
+        return this.salsa;
+    }
+    public void setSalsa(String salsa){
+        this.salsa = salsa;
+    }
+
+    public String getVeggies(){
+        return this.veggies;
+    }
+    public void setVeggies(String veggies){
+        this.veggies = veggies;
+    }
 
 }
--- week2/ChipotleObjectsApp/src/ChipotleObjects.java
+++ week2/ChipotleObjectsApp/src/ChipotleObjects.java
@@ -1,5 +1,7 @@
+
 public class ChipotleObjects {
-    public static void main(String[] args {
+
+    public static void main(String[] args) {
 
     }
 }
