ac366bc93f4fdbda136a0dd0252423acd257fed3
rose-lake <klake12@montgomerycollege.edu>
2019-11-04 12:07:02 -0500

    BookApp: added method to return pricing for 'x' number of books

--- week3/BookApplication/src/Book.java
+++ week3/BookApplication/src/Book.java
@@ -1,20 +1,62 @@
 public class Book {
-    protected String title, author, description;
+    protected String title;
+    protected String author;
+    protected String description;
     protected double price;
     protected boolean inStock;
+    protected int quantityInStock;
 
     // default constructor
     public Book(){};
 
-    // overloaded constructor
-    public Book(String title, String author, String description, double price, boolean inStock){
+    // (partly)overloaded constructor
+    // taking out the inStock parameter on constructor... May add it in later for further functionality,
+    // but at this time its value will be regulated (on construction) by the quantityInStock amount.
+    public Book(String title, String author, String description, double price, int quantityInStock){
         this.title = title;
         this.author = author;
         this.description = description;
         this.price = price;
-        this.inStock = inStock;
+        this.quantityInStock = quantityInStock;
+        if (this.quantityInStock < 1) {
+            this.inStock = false;
+        }
+        else {
+            this.inStock = true;
+        }
     }
 
+    // ************************************************************************
+    // Assignment:
+    // Create a method that returns the pricing for a requested number of books.
+    // Five books at $20.00 should return $100, if they are in stock.
+    // If they are not in stock, that should be handled appropriately (hint - you decide).
+    // ************************************************************************
+    // Implementation:
+    //      numRequested MUST BE an INTEGER > 0
+    //      this method returns price for a given number of this book instance,
+    //          so long as sufficient copies of the book are inStock !
+    //      if book is not in stock return -1
+    //      if a few copies are in stock, but not as many as requested, return -2
+    // ************************************************************************
+    public double getPricing(int numRequested){
+        if (inStock) {
+            if (numRequested <= quantityInStock){
+                return price * numRequested;
+            }
+            else {
+                return -2;
+            }
+        }
+        else {
+            return -1;
+        }
+    }
+
+    // this method displays basic book information to System.out
+    // the Author
+    // the Title
+    // the Description
     public void getDisplayText(){
         System.out.println("Author: " + author + "\nTitle: " + title + "\nDescription: " + description);
     }
@@ -37,6 +79,9 @@ public class Book {
     public boolean isInStock(){
         return inStock;
     }
+    public int getQuantityInStock(){
+        return quantityInStock;
+    }
 
     // *************
     // all setters
@@ -55,6 +100,29 @@ public class Book {
     }
     public void setInStock(boolean inStock){
         this.inStock = inStock;
+    }
+    public void setQuantityInStock(int quantityInStock){
+        if (quantityInStock < 1) {
+            this.inStock = false;
+        }
+        else {
+            this.inStock = true;
+        }
+        this.quantityInStock = quantityInStock;
+    }
+
+    //********************
+    // toString override
+    //********************
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", inStock=" + inStock +
+                '}';
     }
 
 }
--- week3/BookApplication/src/BookApp.java
+++ week3/BookApplication/src/BookApp.java
@@ -1,9 +1,83 @@
+import java.util.*;
+
 public class BookApp {
     public static void main(String[] args){
-        Book bornACrime = new Book();
-        bornACrime.setAuthor("Trevor Noah");
-        bornACrime.setTitle("Born A Crime");
-        bornACrime.setDescription("Trevor Noah's autobiography.");
-        bornACrime.getDisplayText();
+
+        ArrayList<Book> bookList = new ArrayList<Book>();
+
+        Book book1 = new Book();
+        book1.setTitle("Born A Crime");
+        book1.setAuthor("Trevor Noah");
+        book1.setDescription("Trevor Noah's autobiography.");
+        book1.setPrice(8.99);
+        book1.setInStock(true);
+        book1.setQuantityInStock(9);
+        bookList.add(book1);
+        Book book2 = new Book("Diary of a Wimpy Kid: Wrecking Ball", "Jeff Kinney",
+                "An unexpected inheritance gives Greg Heffley’s family a chance to make big changes to their house",
+                9.54, 8);
+        Book book3 = new Book("The Giver of Stars: A Novel", "Jojo Moyes",
+                "Set in Depression-era America, a breathtaking story of five extraordinary women and their remarkable journey.",
+                21.49, 5);
+        Book book4 = new Book("Where the Crawdads Sing", "Delia Owens",
+                "At once an exquisite ode to the natural world, a heartbreaking coming-of-age story, and a surprising tale of possible murder.",
+                15.39, 3);
+        Book book5 = new Book("The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma", "Bessel van der Kolk, M.D.",
+                "Exposes the tremendous power of our relationships both to hurt and to heal—and offers new hope for reclaiming lives.",
+                10.88, 1);
+        Collections.addAll(bookList, book2, book3, book4, book5);
+
+        System.out.println("**************************************");
+        System.out.println("Assignment 1:\n");
+        book1.getDisplayText();
+        System.out.println("\n");
+
+        System.out.println("**************************************");
+        System.out.println("Assignment 2: Simple, for ONE book\n");
+        requestBook(book1, 8);
+        requestBook(book1, 10);
+        Book outOfStockBook = new Book("This is a title", "This is an Author", "This book is about something", 1.99, 0);
+        requestBook(outOfStockBook, 1);
+        Book anotherOutOfStockBook = new Book("This is ANOTHER title", "This is ANOTHER Author", "This book is about SOMETHING", 2.99, -2);
+        requestBook(anotherOutOfStockBook, 1);
+
+        System.out.println("**************************************");
+        System.out.println("Assignment 2: More Complicated, for a HashMap of books and their requested quantities\n");
+        HashMap<Book, Integer> bookRequestMap= new HashMap<Book, Integer>();
+        Random randomGenerator = new Random();
+        for(Book book : bookList) {
+            int rand = randomGenerator.nextInt(15) + 1;     // random request value from 1 to 15
+            bookRequestMap.put(book, rand);
+        }
+        // from:  https://www.geeksforgeeks.org/iterate-map-java/   option 4: using forEach(action) and a lambda
+        bookRequestMap.forEach((book, request) -> requestBook(book, request));
+
+        // interpreting IntelliJ's hint on above statement...
+        System.out.println("**************************************");
+        System.out.println("Trying Assignment 2 again with sepcial 'method reference' syntax: ");
+        bookRequestMap.forEach(BookApp::requestBook);
+        // yep! It works! Gives exact same output as the first call to the method. Cool...
+
+    } // end main()
+
+    private static void requestBook(Book book, int quantity) {
+        // simple implementation: for ONE book instance at a time:
+        double price = book.getPricing(quantity);
+        System.out.println("You have requested " + quantity + " copies of the book '" + book.getTitle() + "'");
+        if (price > 0) { // sufficient copies of the book in stock !
+            System.out.printf("The total cost will be: $%.2f%n%n", price);
+        }
+        else if (price == -1) {
+            System.out.println("We're sorry. That book is out of stock.");
+            System.out.println("isInStock returns: " + book.isInStock() + " and quantityInstock is: " + book.getQuantityInStock() + "\n");
+        }
+        else if (price == -2) {
+            System.out.println("We're sorry, there are insufficient copies of that book available.");
+            System.out.println("There are only " + book.getQuantityInStock() + " copies available.\n");
+        }
+        else {
+            System.out.println("Sorry. Unknown response. book.getPricing("+quantity+") returned: " + price + "\n");
+        }
     }
-}
+
+} // end BookApp class
